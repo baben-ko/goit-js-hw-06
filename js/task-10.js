f89c882cbec276ec6c8accb627b0bc88c0d4e8fa
Andrii_Babenko <andrey.brnr@gmail.com>
2021-01-18 16:38:09 +0200

    main.js - DONE

--- js/task-10.js
+++ js/task-10.js
@@ -1,8 +1,12 @@
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+import users from "./users.js"
 
-const getSortedUniqueSkills = users => {
-  // твой код
-};
+const getSortedUniqueSkills = users =>
+  users
+    .map(users => users.skills)
+    .reduce((allSkills, user) => [...allSkills, ...user], [])
+    .filter((elem, index, arr) => arr.indexOf(elem) === index)
+    .sort();
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
